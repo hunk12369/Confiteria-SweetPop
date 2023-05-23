d4e811661eb26ab380138d8bd75baed4557b0527
hunk12369 <kevinbryanmora2@hotmail.com>
2023-05-22 20:36:53 -0400

    inico del proyecto

---
+++ src/assets/components/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Home.css"
+const Home = () => {
+  return (
+    <div>
+      <h1>
+        INICIO
+      </h1>
+      <h2>
+        SweetPop
+      </h2>
+    </div>
+  )
+}
+
+export default Home
