833fb0155d80211d735d048509af7475d892ed18
hunk12369 <kevinbryanmora2@hotmail.com>
2023-05-26 20:35:18 -0400

    home inicio

---
+++ src/pages/Home.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import "./Home.css"
+const Home = () => {
+  return (
+    <div>
+      
+      <div className='home-encabezado'>
+        <h1>
+          INICIO
+        </h1>
+        <h2>
+          <span className='subrayado'>Sweet</span>Pop
+        </h2>
+        <img src="/src/assets/images/transparent-sanrio-cinnamoroll-cinnamon-animals-kawaii-cinnamoroll 2.png" alt="imagne del home"></img>
+      </div>
+      <div className='olas'>
+        <div className='ola'></div>
+        <div className='ola'></div>
+      </div>
+
+      <div className='servicios'>
+        <p>hola</p>
+
+      </div>
+      
+    </div>
+  )
+}
+
+export default Home
