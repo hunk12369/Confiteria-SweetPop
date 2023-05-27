25bfe028763317d5a2acee0dd672d1cfd5d57164
hunk12369 <kevinbryanmora2@hotmail.com>
2023-05-26 20:29:03 -0400

    arreglando carpetas

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import Home from './assets/components/Home'
-import './App.css'
+import Home from './pages/Home'
+
 
 function App() {
   const [count, setCount] = useState(0)
