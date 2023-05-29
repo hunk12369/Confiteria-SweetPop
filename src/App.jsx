15044f20c1988a26ac69dab1f4a39c98676d04e4
carlos <pachecoc010@gmail.com>
2023-05-29 17:52:43 -0400

    fix:tamaño de la pagina contactos

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 import { useState } from 'react'
-import Home from './pages/Home.jsx'
+import Contact from './components/Contact'
 import Contactos from './pages/Contactos'
-import OlasAdorno from './components/OlasAdorno.jsx'
-import Fotter from './components/Fotter.jsx'
-import Header from './components/Header.jsx'
-import Information from './components/Information.jsx'
-import NetworkButtons from './components/NetworkButtons.jsx'
+
+
 
 
 
@@ -15,12 +12,13 @@ function App() {
 
   return (
     <>
-      <Contactos/>
-      <Fotter/>
-      <Header/>
-      <Information/>
-      <NetworkButtons/>
-         
+    <div></div>
+    <Contactos/>
+    
+    
+     
+     
+     
     </>
   )
 }
