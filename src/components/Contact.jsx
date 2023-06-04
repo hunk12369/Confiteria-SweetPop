4718205386db10d0a06d192ecbbca08cdd96e809
carlos <pachecoc010@gmail.com>
2023-06-03 22:41:07 -0400

    fix:iconos

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -19,11 +19,15 @@ const Contact = () => {
     <div className='relleno1'>
         <img src="src/assets/images/1.jpg" alt="" />
     </div>
+    <div className='olasContactanos'>
     <OlasAdorno/>
+    </div>
     <div className='relleno2'>
         <img src="src/assets/images/masas1.jpg" alt="" />
     </div>
+    <div className='fotoPortada'>
     <img src="src/assets/images/2.jpg" alt=''/>
+    </div>
     <Fotter/>
     <Information/>
     <NetworkButtons/>
