3fe7f5ae3886a6547cf340d4629d0ce3e0b99c37
hunk12369 <kevinbryanmora2@hotmail.com>
2023-06-04 18:13:25 -0400

    rev: Registro

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,15 @@ import Contact from './components/Contact'
 import Contactos from './pages/Contactos'
 import Home from './pages/Home'
 import Categorias from './pages/Categorias'
-
+import Login from './components/Login'
+import CrearCuenta from './components/CrearCuenta'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <Categorias/>
+    <CrearCuenta/>
     </>
   )
 }
