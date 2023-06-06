c43f955721b5c4583ef915a6572c04d85b3af8f7
hunk12369 <kevinbryanmora2@hotmail.com>
2023-06-06 16:52:19 -0400

    feat:routs

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom'
+import Home from './pages/Home.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App/>}/>
+        
+        <Route path='*' element={<p>Page Not found</p>}/>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
