c6dd06d52cf1015e4c88959f9c7fcdb71870518f
hunk12369 <kevinbryanmora2@hotmail.com>
2023-06-08 22:50:55 -0400

    merge: contactos

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,13 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter, Route, Router, Routes } from 'react-router-dom'
 import Home from './pages/Home.jsx'
+import Contactos from './pages/Contactos.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<App/>}/>
-        
+        <Route path='/' element={<Home/>}/>
+        <Route path='/contactos' element={<Contactos/>}/>
         <Route path='*' element={<p>Page Not found</p>}/>
       </Routes>
     </BrowserRouter>
