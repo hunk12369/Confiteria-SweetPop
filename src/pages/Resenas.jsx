fab5dd9cd1e2bd85a2d2dc618b3277ea7c9701f3
hunk12369 <kevinbryanmora2@hotmail.com>
2023-06-12 00:02:17 -0400

    fix: erro de merge

---
+++ src/pages/Resenas.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Review from '../components/Review'
+
+const Resenas = () => {
+  return (
+    <div>
+        <Review/>
+    </div>
+  )
+}
+
+export default Resenas
